a75e00a0c4cfd94b7cc20afe7493281ce82ca945
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 05:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index do |x,num|
+    puts "#{num+1} #{x}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(list)
+  
 end
 
 def long_planeteer_calls# code an argument here
